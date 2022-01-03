93b2c84b6fd8e38984710c7c7a5f6717c9c36473
Hugo Oliveira <hugo684@gmail.com>
2022-01-03 18:25:44 -0300

    Created UserAgent and OrderAgent

---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,15 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users
+  alias Users.Agent, as: UserAgent
+  alias Users.User
+
+  def call(%{name: name, address: address, email: email, cpf: cpf, age: age}) do
+    User.build(name, email, address, cpf, age)
+    |> save_user()
+  end
+
+  def call(_), do: {:error, "Invalid parameters!"}
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:error, _detail} = error), do: error
+end
